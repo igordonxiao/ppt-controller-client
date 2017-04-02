93692e2931d9641bdfefb8a74e856698974d497e
igordonshaw@gmail.com <mongkon8257>
2017-04-02 21:57:43 +0800

    clean code

--- src/main/java/io/igordonxiao/app/PPTApplication.kt
+++ src/main/java/io/igordonxiao/app/PPTApplication.kt
@@ -19,17 +19,21 @@ import javax.swing.JTextArea
  */
 
 fun main(args: Array<String>) {
-    val frame = JFrame("PPT Controller")
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    val panel = JPanel()
-    val infoArea = JTextArea()
-    panel.layout = GridLayout()
-    infoArea.text = "开始准备连接服务器......\n"
+    val panel = JPanel().apply {
+        layout = GridLayout()
+    }
+    val frame = JFrame("PPT Controller").apply {
+        add(panel)
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        setSize(300, 300)
+        setLocationRelativeTo(null)
+        isVisible = true
+    }
+
+    val infoArea = JTextArea().apply {
+        text = "开始准备连接服务器......\n"
+    }
     panel.add(JScrollPane(infoArea))
-    frame.add(panel)
-    frame.setSize(300, 300)
-    frame.setLocationRelativeTo(null)
-    frame.isVisible = true
 
     // Robot
     val robot = Robot()
@@ -42,41 +46,28 @@ fun main(args: Array<String>) {
     val url = "ws://pptcontroller.herokuapp.com/ws"
     val req = Request.Builder().url(url).build()
     val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
     client.newWebSocket(req, object : WebSocketListener() {
         override fun onOpen(webSocket: WebSocket, response: Response) {
             webSocket.send("client: subscription")
         }
 
-        override fun onMessage(webSocket: WebSocket, text: String) {
-            //infoArea.append("\n收到: $text")
-            when (text) {
-                "cmd:up" -> {
-                    infoArea.append("\n${dateFormat.format(Date())} 上一页")
-                    robot.keyPress(KeyEvent.VK_UP)
-                }
-                "cmd:down" -> {
-                    infoArea.append("\n${dateFormat.format(Date())} 下一页")
-                    robot.keyPress(KeyEvent.VK_DOWN)
-                }
-                else -> {
-                }
+        override fun onMessage(webSocket: WebSocket, text: String) = when (text) {
+            "cmd:up" -> {
+                infoArea.append("\n${dateFormat.format(Date())} 上一页")
+                robot.keyPress(KeyEvent.VK_UP)
+            }
+            "cmd:down" -> {
+                infoArea.append("\n${dateFormat.format(Date())} 下一页")
+                robot.keyPress(KeyEvent.VK_DOWN)
+            }
+            else -> {
             }
         }
 
-        override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-            infoArea.append("\n-收到: $bytes")
-        }
-
-        override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
-            infoArea.append("\n连接关闭")
-        }
-
-        override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-            infoArea.append("\n连接关闭：$reason")
-        }
-
-        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response) {
-            infoArea.append("\n连接失败，请重试")
-        }
+        override fun onMessage(webSocket: WebSocket, bytes: ByteString) = infoArea.append("\n-收到: $bytes")
+        override fun onClosing(webSocket: WebSocket, code: Int, reason: String) = infoArea.append("\n连接关闭")
+        override fun onClosed(webSocket: WebSocket, code: Int, reason: String) = infoArea.append("\n连接关闭：$reason")
+        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response)= infoArea.append("\n连接失败，请重试")
     })
 }
